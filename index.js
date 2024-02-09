c8651a8bbb74721463e2d932d19e8fffa92908be
Ikramullah <64439464+Ikramullah-Khateeb@users.noreply.github.com>
2024-02-09 22:16:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+ let saveEl = document.getElementById("save-el");
+let countEl = document.getElementById("count-el")
+let count =0 ;
+
+console.log(countEl)
+
+console.log(saveEl);
+function increment(){
+    count += 1
+    countEl.textContent = count
+   }
+   function save(){
+       let countStr= count + " - "
+   saveEl.textContent += countStr
+       console.log(count)
+   }
+   
+   
+  
+   
+  
+   
+
+
+
+
+
+
+
+
+
+
+
